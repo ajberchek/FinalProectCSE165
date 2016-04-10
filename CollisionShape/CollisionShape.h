3128d3f51be678fd2cfa31e6abfc393914be3469
ajberchek <ajberchek@gmail.com>
2016-04-10 16:25:31 -0700

    Added interface for CollisionShape

---
+++ CollisionShape/CollisionShape.h
@@ -0,0 +1,21 @@
+#ifndef COLLIDEABLESHAPE_H
+#define COLLIDEABLESHAPE_H
+#include <math.h>
+class CollideableShape
+{
+	private:
+		float x,y,width,height; //see subclasses for what each one of these correspond to.
+	public:
+		virtual bool isOverlap(const CollideableShape * cs) = 0;
+		void update(float _x, float _y, float _w, float _h)
+		{
+			x= _x;
+			y = _y;
+			width = _w;
+			height = _h;
+		}
+};
+
+
+
+#endif

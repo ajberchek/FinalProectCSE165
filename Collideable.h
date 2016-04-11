8e2ea00c763a99a97254cac0af8eb5d5f75b6499
punjabiputt29 <punjabiputt29@gmail.com>
2016-04-10 17:23:50 -0700

    added getters in colideable.h and changed some of hang's code, and commented on which one i changed

--- Collideable.h
+++ Collideable.h
@@ -1,24 +1,23 @@
 #ifndef COLLIDEABLE_H
 #define COLLIDEABLE_H
 #include <vector>
-//#include "CollisionShape.h"
-//#include "Animation.h"
+#include "CollisionShape.h"
+#include "Animation.h"
 
 class Collideable
 {
 	float x, y, w, h, maxSpeed;
-	//vector<Animation *> anim;
-	//CollisionShape collisionBox;
+	vector<Animation *> anim;
+	CollisionShape collisionBox;
 	public:
 		Collideable();
 		virtual void onCollision(const Collideable & c) = 0;
-		//virtual draw(const Animation  & a) = 0;				//changed from Animation *
-		void update(float x = x, float y = y, float w = w, float h = h);
+		virtual draw(const Animation  & a) = 0;				//changed from Animation *
+		void update(float x, float y, float w, float h);	//originally by Hang as (float x = x, float y = y, float w = w, float h = h);
 		int getX();
 		int getY();
 		int getW();
 		int getH();
 		int getSpeed();
-
 };
 #endif

3d2ad4b398d8d3c360d4fa018322abc8ab7c969a
Ditto <vinh9015@gmail.com>
2016-05-03 16:44:32 -0700

    Optimized Animation.cpp to remove overflow 2

--- AnimationFactory.d
+++
@@ -1,3 +0,0 @@
-AnimationFactory.o: AnimationFactory.cpp AnimationFactory.h Coin.h \
- Collideable.h CollisionShape.h Animation.h Door.h Stats.h Mole.h \
- Player.h Wall.h SOIL.h
--- CollisionShapeCircle.d
+++
@@ -1,2 +0,0 @@
-CollisionShapeCircle.o: CollisionShapeCircle.cpp CollisionShapeCircle.h \
- CollisionShape.h CollisionShapeRect.h
